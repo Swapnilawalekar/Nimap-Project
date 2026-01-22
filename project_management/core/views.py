1add040e0253fe669b29f2402cf584b4d57ca48d
Swapnilawalekar <swapnilawalekar18@gmail.com>
2026-01-22 11:12:32 +0530

    Updated

---
+++ project_management/core/views.py
@@ -0,0 +1,50 @@
+from rest_framework import viewsets, generics, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from .models import Client, Project
+from .serializers import (
+    ClientSerializer,
+    ProjectSerializer,
+    ProjectCreateSerializer
+)
+from django.shortcuts import get_object_or_404
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def projects(self, request, pk=None):
+        """
+        Create a new project under this client and assign users to it
+        POST /clients/:id/projects/
+        """
+        client = self.get_object()
+        serializer = ProjectCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            project = serializer.save(client=client, created_by=request.user)
+            project.users.set(serializer.validated_data['users'])
+            return Response(ProjectSerializer(project).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserProjectsView(generics.ListAPIView):
+    """
+    GET /projects/
+    List of all projects assigned to the logged-in user
+    """
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.filter(users=self.request.user)
+    
+
+    #this is updated code
